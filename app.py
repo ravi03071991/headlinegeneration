ac8fdffa847d20b4e887402e4f21f89a1aedded7
Ravi Theja <ravi03071991@gmail.com>
2023-02-09 12:33:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.write("This app will generate headlines based on text with limits on number o
 
 def generate(text, num_chars, num_words):
 
-    openai.api_key = "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"
+    openai.api_key = "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"
     query = f"Generate a news headline that will attract users attention strict to maximum of {num_chars} characters and {num_words} words without : based on the context of the following text. Don't hallicunate."
 
     response = openai.Completion.create(
